237dec4dfc75ab214b7d2ed97dbfc78c1f4830bd
arnaucode <idoctnef@openmailbox.org>
2017-02-01 18:14:38 +0100

    pagination added to travels feed

--- www/js/travels.js
+++ www/js/travels.js
@@ -4,14 +4,15 @@ angular.module('app.travels', ['pascalprecht.translate'])
 
 
     $scope.travels=[];
-
+    $scope.page=0;
     $scope.doRefresh = function() {
       /* travels refresh: */
-        $http.get(urlapi + 'travels')
+        $http.get(urlapi + 'travels?page=' + $scope.page)
         .then(function(data){
             console.log('data success travels');
             console.log(data); // for browser console
-            $scope.travels = data.data; // for UI
+            //$scope.travels = data.data; // for UI
+            $scope.travels=$scope.travels.concat(data.data);
             $scope.$broadcast('scroll.refreshComplete');//refresher stop
 
         }, function(data){
@@ -22,4 +23,10 @@ angular.module('app.travels', ['pascalprecht.translate'])
         });
     };
     $scope.doRefresh();
+
+    $scope.paginationNext = function(){
+        $scope.page++;
+        console.log($scope.page);
+        $scope.doRefresh();
+    };
 });
